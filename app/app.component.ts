929d2aff088a95420de542250aee3d54b78e7160
Josiah Berkebile <praenato14@gmail.com>
2017-04-20 21:45:00 -0500

    2.8 Images

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from "@angular/core";
   selector: "my-app",
   template: `
     <StackLayout>
+      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
       <TextField hint="Email Address" keyboardType="email"
         autocorrect="false" autocapitalizationType="none"></TextField>
       <TextField hint="Password" secure="true"></TextField>
