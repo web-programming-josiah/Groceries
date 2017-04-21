62f0787a953f30cf764d39f7f522556f5d3407a7
Josiah Berkebile <praenato14@gmail.com>
2017-04-21 17:20:49 -0500

    3.5 Routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from "@angular/core";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NativeScriptModule } from "nativescript-angular/platform";
 import { NativeScriptHttpModule } from "nativescript-angular";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
 
 import { AppComponent } from "./app.component";
+import { routes, navigableComponents } from "./app.routing";
 
 @NgModule({
   imports: [
     NativeScriptModule,
     NativeScriptFormsModule,
-    NativeScriptHttpModule
+    NativeScriptHttpModule,
+    NativeScriptRouterModule,
+    NativeScriptRouterModule.forRoot(routes)
   ],
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ...navigableComponents
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- app/shared/user/user.service.ts
+++ app/shared/user/user.service.ts
@@ -27,6 +27,26 @@ export class UserService {
     .catch(this.handleErrors);
   }
 
+  login(user: User) {
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    return this.http.post(
+      Config.apiUrl + "oauth/token",
+      JSON.stringify({
+        username: user.email,
+        password: user.password,
+        grant_type: "password"
+      }),
+      { headers: headers }
+    )
+    .map(response => response.json())
+    .do(data => {
+      Config.token = data.Result.access_token;
+    })
+    .catch(this.handleErrors);
+  }
+
   handleErrors(error: Response) {
     console.error(JSON.stringify(error.json()));
     return Observable.throw(error);
