c3cb1c4f33eeb6bd4819f8d9226dff04c9ebdeee
Josiah Berkebile <praenato14@gmail.com>
2017-04-20 21:38:16 -0500

    2.7 Component Specific CSS

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,9 +8,10 @@ import { Component } from "@angular/core";
         autocorrect="false" autocapitalizationType="none"></TextField>
       <TextField hint="Password" secure="true"></TextField>
 
-      <Button text="Sign in"></Button>
+      <Button text="Sign in" class="submit-button"></Button>
       <Button text="Sign up for Groceries"></Button>
     </StackLayout>
-  `
+  `,
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
 export class AppComponent {}
